1f6a05bc386a8e2ee3a699072452436e2970bc40
canyon <48212912+saucecan@users.noreply.github.com>
2022-12-07 15:42:56 -0500

    Update UniqueWords.java

--- Lab12/src/e/csxx/UniqueWords.java
+++ Lab12/src/e/csxx/UniqueWords.java
@@ -11,16 +11,20 @@ import java.util.HashSet;
  * @author 
  */
 public class UniqueWords {
-    public static Set<String> processDocument(File myfile) throws IOException {
+    public static Set<String> processDocument(File myfile) throws FileNotFoundException {
         Scanner file = new Scanner(myfile);
-        HashSet<String> h = new HashSet<>();
-        while (file.hasNextLine()) {
-            String line = file.nextLine();
-            String[] tokenizedLine = tokenize(line);
-            h.addAll(Arrays.asList(tokenizedLine));
+        try {
+                HashSet<String> h = new HashSet<>();
+                while (file.hasNextLine()) {
+                    String line = file.nextLine();
+                    String[] tokenizedLine = tokenize(line);
+                    h.addAll(Arrays.asList(tokenizedLine));
+                }
+                TreeSet<String> t = new TreeSet<>(h);
+                return t;
+        } finally {
+            file.close();
         }
-        h = sortHash(h);
-        return h;
     }
 
     public static String[] tokenize(String str) {
@@ -30,12 +34,5 @@ public class UniqueWords {
             tok[i] = tok[i].toLowerCase();
         }
         return tok;
-    }
-
-    public static LinkedHashSet<String> sortHash(HashSet<String> h) {
-        List<String> List1 = new ArrayList<String>(h);
-        Collections.sort(List1);
-        LinkedHashSet<String> sortedh = new LinkedHashSet<>(List1);
-        return sortedh;
     }
 }
